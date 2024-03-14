1dafeda80a5b55a9512985d2aaee3612d8bc81b8
Vitor Ibraim De Paula Reis <vitor.reis@conciliadora.local>
2024-03-14 11:19:24 -0300

    First commit

---
+++ Exercicios7/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicios7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escolha um numero para apresentar sua tabuada: ");
+            int Choices = int.Parse(Console.ReadLine());
+            switch (Choices)
+            {
+                case 1:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+
+                    Console.WriteLine(1 * 1);
+                    Console.WriteLine(1 * 2);
+                    Console.WriteLine(1 * 3);
+                    Console.WriteLine(1 * 4);
+                    Console.WriteLine(1 * 5);
+                    Console.WriteLine(1 * 6);
+                    Console.WriteLine(1 * 7);
+                    Console.WriteLine(1 * 8);
+                    Console.WriteLine(1 * 9);
+                    Console.WriteLine(1 * 10);
+                    break;
+                case 2:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(2 * 1);
+                    Console.WriteLine(2 * 2);
+                    Console.WriteLine(2 * 3);
+                    Console.WriteLine(2 * 4);
+                    Console.WriteLine(2 * 5);
+                    Console.WriteLine(2 * 6);
+                    Console.WriteLine(2 * 7);
+                    Console.WriteLine(2 * 8);
+                    Console.WriteLine(2 * 9);
+                    Console.WriteLine(2 * 10);
+                    break;
+                case 3:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(3 * 1);
+                    Console.WriteLine(3 * 2);
+                    Console.WriteLine(3 * 3);
+                    Console.WriteLine(3 * 4);
+                    Console.WriteLine(3 * 5);
+                    Console.WriteLine(3 * 6);
+                    Console.WriteLine(3 * 7);
+                    Console.WriteLine(3 * 8);
+                    Console.WriteLine(3 * 9);
+                    Console.WriteLine(3 * 10);
+                    break;
+                case 4:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(4 * 1);
+                    Console.WriteLine(4 * 2);
+                    Console.WriteLine(4 * 3);
+                    Console.WriteLine(4 * 4);
+                    Console.WriteLine(4 * 5);
+                    Console.WriteLine(4 * 6);
+                    Console.WriteLine(4 * 7);
+                    Console.WriteLine(4 * 8);
+                    Console.WriteLine(4 * 9);
+                    Console.WriteLine(4 * 10);
+                    break;
+                case 5:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(5 * 1);
+                    Console.WriteLine(5 * 2);
+                    Console.WriteLine(5 * 3);
+                    Console.WriteLine(5 * 4);
+                    Console.WriteLine(5 * 5);
+                    Console.WriteLine(5 * 6);
+                    Console.WriteLine(5 * 7);
+                    Console.WriteLine(5 * 8);
+                    Console.WriteLine(5 * 9);
+                    Console.WriteLine(5 * 10);
+                    break;
+                case 6:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(6 * 1);
+                    Console.WriteLine(6 * 2);
+                    Console.WriteLine(6 * 3);
+                    Console.WriteLine(6 * 4);
+                    Console.WriteLine(6 * 5);
+                    Console.WriteLine(6 * 6);
+                    Console.WriteLine(6 * 7);
+                    Console.WriteLine(6 * 8);
+                    Console.WriteLine(6 * 9);
+                    Console.WriteLine(6 * 10);
+                    break;
+                case 7:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(7 * 1);
+                    Console.WriteLine(7 * 2);
+                    Console.WriteLine(7 * 3);
+                    Console.WriteLine(7 * 4);
+                    Console.WriteLine(7 * 5);
+                    Console.WriteLine(7 * 6);
+                    Console.WriteLine(7 * 7);
+                    Console.WriteLine(7 * 8);
+                    Console.WriteLine(7 * 9);
+                    Console.WriteLine(7 * 10);
+                    break;
+                case 8:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(8 * 1);
+                    Console.WriteLine(8 * 2);
+                    Console.WriteLine(8 * 3);
+                    Console.WriteLine(8 * 4);
+                    Console.WriteLine(8 * 5);
+                    Console.WriteLine(8 * 6);
+                    Console.WriteLine(8 * 7);
+                    Console.WriteLine(8 * 8);
+                    Console.WriteLine(8 * 9);
+                    Console.WriteLine(8 * 10);
+                    break;
+                case 9:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(9 * 1);
+                    Console.WriteLine(9 * 2);
+                    Console.WriteLine(9 * 3);
+                    Console.WriteLine(9 * 4);
+                    Console.WriteLine(9 * 5);
+                    Console.WriteLine(9 * 6);
+                    Console.WriteLine(9 * 7);
+                    Console.WriteLine(9 * 8);
+                    Console.WriteLine(9 * 9);
+                    Console.WriteLine(9 * 10);
+                    break;
+                case 10:
+                    Console.WriteLine("O numero que voce escolheu foi:" + Choices);
+
+                    Console.WriteLine(10 * 1);
+                    Console.WriteLine(10 * 2);
+                    Console.WriteLine(10 * 3);
+                    Console.WriteLine(10 * 4);
+                    Console.WriteLine(10 * 5);
+                    Console.WriteLine(10 * 6);
+                    Console.WriteLine(10 * 7);
+                    Console.WriteLine(10 * 8);
+                    Console.WriteLine(10 * 9);
+                    Console.WriteLine(10 * 10);
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
